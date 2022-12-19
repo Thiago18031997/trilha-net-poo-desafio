f0d1b0c1427181ecd5296b8bb449ec084cddccf6
Thiago18031997 <thiagosantossouza92@gmail.com>
2022-12-19 18:35:24 -0300

    Desafio concluído com sucesso!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
-
+using static System.Console;
 // TODO: Realizar os testes com as classes Nokia e Iphone
+WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("124536", "Modelo 1", "196516519", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Google Chrome");
+
+WriteLine();
+WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("6541951951", "Modelo 2", "2222222222", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Dio.me");
